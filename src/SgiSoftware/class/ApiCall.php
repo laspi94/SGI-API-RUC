0277ce79e7d82ec05b9419b13247c155966ecf3b
Alan Laspina <alanjoselaspina@gmail.com>
2023-06-23 17:56:56 -0400

    Se añade archivo de configuración

--- src/SgiSoftware/class/ApiCall.php
+++ src/SgiSoftware/class/ApiCall.php
@@ -30,35 +30,11 @@ class ApiCall
      */
     public function __construct()
     {
-        $config =  self::getConfig();
-
-        $this->APIKEY = env('RUC_APIKEY', '');
-        $this->API_BASE_PATH = $config['sgi_ruc_base_path'];
-        $this->API_BASE_PATH_ENDPOINT = $config['sgi_ruc_api_base_path_endpoint'];
-        $this->API_ENDPOINT_OBTENER = $config['sgi_ruc_api_endpoint_obtener'];
-        $this->API_ENDPOINT_BUSCAR = $config['sgi_ruc_api_endpoint_buscar'];
-    }
-
-    public static function getConfig()
-    {
-        return [
-            /*
-            |--------------------------------------------------------------------------
-            | DATOS_SET*
-            |--------------------------------------------------------------------------
-            |
-            | Estas variables corresponde a la conexión con el API para la obtención de 
-            | datos de contribuyentes de la SET, el BASE PATH depende del entorno el cual
-            | se esté ejecutando.
-            |
-            */
-
-            'ruc_apikey' => env('RUC_APIKEY', ''),
-            'ruc_base_path' => env('RUC_API_BASE_PATH', ''),
-            'ruc_api_base_path_endpoint' => env('RUC_API_BASE_PATH_ENDPOINT', ''),
-            'ruc_api_endpoint_obtener' => env('RUC_API_ENDPOINT_OBTENER', ''),
-            'ruc_api_endpoint_buscar' => env('RUC_API_ENDPOINT_BUSCAR', ''),
-        ];
+        $this->APIKEY = config('sgi_ruc_apikey');
+        $this->API_BASE_PATH = config('sgi_ruc_base_path');
+        $this->API_BASE_PATH_ENDPOINT = config('sgi_ruc_api_base_path_endpoint');
+        $this->API_ENDPOINT_OBTENER = config('sgi_ruc_api_endpoint_obtener');
+        $this->API_ENDPOINT_BUSCAR = config('sgi_ruc_api_endpoint_buscar');
     }
 
     public function obtenerContribuyente($rucn)
