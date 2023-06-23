df5f9f7e6593c521459ccb949f5ee1e862a05aa3
Alan Laspina <alanjoselaspina@gmail.com>
2023-06-23 14:34:21 -0400

    Se actualizan requerimientos y autoload file

---
+++ src/helpers/HttpStatus.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * User: alaspina
+ * Date: 12-Feb-2023
+ * Time: 16:49
+ */
+
+namespace src\Helpers;
+
+class StatusHelper
+{
+    static function getStatusCodeText($status)
+    {
+        switch ($status) {
+            case Response::STATUS_LOGIN_TIMEOUT;
+                $statusText = 'LOGIN TIME-OUT';
+                break;
+            case Response::STATUS_UNAUTHORIZE;
+                $statusText = 'UNAUTHORIZE';
+                break;
+            case Response::STATUS_BAD_REQUEST;
+                $statusText = 'BAD REQUEST';
+                break;
+            case Response::STATUS_NOT_FOUND;
+                $statusText = 'NOT FOUND';
+                break;
+            case Response::STATUS_OK;
+                $statusText = 'OK';
+                break;
+            case Response::STATUS_FORBIDDEN;
+                $statusText = 'FORBIDDEN';
+                break;
+        }
+
+        return $statusText;
+    }
+}
